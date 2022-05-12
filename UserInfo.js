022c24ded3e67600fdb2a76a83f939ea8582c772
TruongDuyKhai <102969311+TruongDuyKhai@users.noreply.github.com>
2022-05-12 21:15:23 +0700

    Create UserInfo.js
    User Information

---
+++ UserInfo.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'userinfo',
+  aliases: ['ui'],
+  description: 'user information',
+  async execute(client, message, args) {
+    const tdk = require('ezc-tdk');
+    // npm i ezc-tdk
+    const user = message.mentions.users.first() || message.author;
+    // author or mention
+    const member = message.guild.members.cache.get(user.id);
+    
+    const embed = tdk.UserInfo({
+      member: member,
+    });
+    
+    message.channel.send( { embeds: [embed] } );
+  }
+}
